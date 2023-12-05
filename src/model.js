f4ce79eace9f449ea2d9f9da9dcfe450f97f40df
Reza Hosseini <rezahos@kth.se>
2023-12-05 15:36:10 +0100

    add broken bird view

--- src/model.js
+++ src/model.js
@@ -37,15 +37,15 @@ export default {
     599, 601, 602, 603, 605, 606, 608, 610, 616, 617, 618, 621, 622, 623, 624,
     625, 626, 627, 628, 631, 633, 634, 638, 639, 642, 646, 647, 657, 659, 660,
     754, 757, 761, 763, 857, 860, 862, 863, 866, 952, 954, 955, 964, 970, 975,
-    979, 988, 990, 992, 994, 995, 404, 69, 26, 10
+    979, 988, 990, 992, 994, 995, 404, 69, 26, 10,
   ],
 
   setCurrentBird(id) {
-   /* getBirdDetails(id)
+    /* getBirdDetails(id)
       .then((res) => res.json())
       .then((res) => (this.currentBird = res));*/
-      resolvePromise(getBirdDetails(id), this.currentBirdPromiseState);
-      this.currentBird = id;
+    resolvePromise(getBirdDetails(id), this.currentBirdPromiseState);
+    this.currentBird = id;
   },
 
   addLikedBird(bird) {
@@ -63,10 +63,12 @@ export default {
   async setBirdOfTheDay() {
     const getDateOfYearCB = () => {
       const date = new Date();
-      return Math.floor((date.getTime() - new Date(date.getFullYear(), 0, 0)) / 864e5);
-    }
+      return Math.floor(
+        (date.getTime() - new Date(date.getFullYear(), 0, 0)) / 864e5
+      );
+    };
 
-    if(!this.birdOfTheDay) {
+    if (!this.birdOfTheDay) {
       const id = this.birdsOfTheDay[getDateOfYearCB()];
       resolvePromise(getBirdDetails(id), this.birdOfTheDayPromiseState);
       this.birdOfTheDay = id;
@@ -86,6 +88,6 @@ export default {
   },*/
 
   doSearch(searchParams) {
-    resolvePromise(searchBird(searchParams), this.searchResultsPromiseState)
-  }
+    resolvePromise(searchBird(searchParams), this.searchResultsPromiseState);
+  },
 };
