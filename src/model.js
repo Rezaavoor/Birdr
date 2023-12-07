b33cb595bf011249860454247d89bea5f2c49f9a
Ponn3Ponn3 <ponne.ryden@gmail.com>
2023-12-07 09:19:28 +0100

    Add hasImg to search

--- src/model.js
+++ src/model.js
@@ -77,6 +77,10 @@ export default {
     this.searchParams.name = name;
   },
 
+  setHasImg(hasImg) {
+    this.searchParams.hasImg = hasImg;
+  },
+
   /* setSearchRegion(region) {
     this.searchParams.region = region;
   },
@@ -87,5 +91,14 @@ export default {
 
   doSearch(searchParams) {
     resolvePromise(searchBird(searchParams), this.searchResultsPromiseState)
+  },
+
+  isBirdLiked(id) {
+    return this.user.likedBirds.filter(isBirdLikedCB).length > 0;
+
+    function isBirdLikedCB(curId) {
+      return curId == id;
+    }
+
   }
 };
