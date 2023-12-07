c294d59e5511e66b2ce794ba89da52a89956c68f
Reza Hosseini <rezahos@kth.se>
2023-12-07 14:20:10 +0100

    make bird page work (simple version)

--- src/presenters/BirdP.jsx
+++ src/presenters/BirdP.jsx
@@ -1,25 +1,26 @@
+import { observer } from "mobx-react-lite";
 import Bird from "../views/Bird";
 
-export default function BirdP(props) {
+export default observer(function BirdP(props) {
   function onClickAddToMyBirds() {
     props.model.addLikedBird(props.model.birdOfTheDay);
   }
-  if (!props.model.birdOfTheDayPromiseState.promise) {
+  if (!props.model.currentBirdPromiseState.promise) {
     return <Bird status="no data" />;
   }
 
   if (
-    props.model.birdOfTheDayPromiseState.promise &&
-    props.model.birdOfTheDayPromiseState.error &&
-    !props.model.birdOfTheDayPromiseState.data
+    props.model.currentBirdPromiseState.promise &&
+    props.model.currentBirdPromiseState.error &&
+    !props.model.currentBirdPromiseState.data
   ) {
     return <Bird status="error" />;
   }
 
   if (
-    props.model.birdOfTheDayPromiseState.promise &&
-    !props.model.birdOfTheDayPromiseState.error &&
-    !props.model.birdOfTheDayPromiseState.data
+    props.model.currentBirdPromiseState.promise &&
+    !props.model.currentBirdPromiseState.error &&
+    !props.model.currentBirdPromiseState.data
   ) {
     return <Bird status="loading" />;
   }
@@ -31,4 +32,4 @@ export default function BirdP(props) {
       status="data"
     />
   );
-}
+});
--- src/views/Bird.jsx
+++ src/views/Bird.jsx
@@ -3,11 +3,52 @@ import { css } from "@emotion/css";
 import { useTheme } from "@emotion/react";
 import { InfoIcon, AddIcon } from "@chakra-ui/icons";
 
-export default function Bird({ bird, onClickAddToMyBirds, status }) {
+export default function Bird({ bird, status, onClickAddToMyBirds }) {
+  const theme = useTheme();
   return (
-    <div>
+    <div
+      className={css`
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      `}
+    >
       {status == "data" ? (
-        <h1>{bird.name}</h1>
+        <>
+          <div
+            className={css`
+              padding: 20px;
+            `}
+          >
+            <p>Family: {bird.family}</p>
+            {bird.lengthMin && bird.lengthMax && (
+              <p>
+                Size: {bird.lengthMin}-{bird.lengthMax} cm
+              </p>
+            )}
+            {bird.wingspanMin && bird.wingspanMax && (
+              <p>
+                Wing Span: {bird.wingspanMin}-{bird.wingspanMax} cm
+              </p>
+            )}
+
+            {bird.sciName && <p>Scientific Name: {bird.sciName}</p>}
+            {bird.region && <p>Region: {bird.region[0]}</p>}
+            {bird.status && <p>Status: {bird.status}</p>}
+            {bird.order && <p>Order: {bird.order}</p>}
+          </div>
+          <div>
+            <Image
+              src={bird.images[0]}
+              alt={bird.name}
+              objectFit="cover"
+              boxSize={500}
+              fallbackSrc="https://via.placeholder.com/150"
+            />
+          </div>
+        </>
       ) : status == "loading" ? (
         <Spinner size="xl" />
       ) : status == "error" ? (
