cc5469a9fcdd986a156af5e5774e3e1d764cf600
JoakimSundman <itlock993@gmail.com>
2023-12-14 00:34:26 +0100

    Temp fix, useEffect

--- src/presenters/HotlistP.jsx
+++ src/presenters/HotlistP.jsx
@@ -1,37 +1,49 @@
 import Hotlist from "../views/Hotlist";
 import { useNavigate } from "react-router-dom";
 import { observer } from "mobx-react-lite";
+import { useEffect } from "react";
 
 export default observer(function HotlistP(props) {
-  props.model.getHotBirds();
+  const navigate = useNavigate();
 
-  if (!props.model.birdOfTheDayPromiseState.promise) {
+  useEffect(() => {
+    props.model.getHotBirds();
+  }, []);
+
+  function onClickMoreDetails() {
+    props.model.setCurrentBird(props.model.birdOfTheDay);
+    navigate("/bird");
+  }
+
+  if (!props.model.hotBirdsPromiseState.promise) {
     return <Hotlist status="no data" />;
   }
 
   if (
-    props.model.birdOfTheDayPromiseState.promise &&
-    props.model.birdOfTheDayPromiseState.error &&
-    !props.model.birdOfTheDayPromiseState.data
+    props.model.hotBirdsPromiseState.promise &&
+    props.model.hotBirdsPromiseState.error &&
+    !props.model.hotBirdsPromiseState.data
   ) {
     return <Hotlist status="error" />;
   }
 
   if (
-    props.model.birdOfTheDayPromiseState.promise &&
-    !props.model.birdOfTheDayPromiseState.error &&
-    !props.model.birdOfTheDayPromiseState.data
+    props.model.hotBirdsPromiseState.promise &&
+    !props.model.hotBirdsPromiseState.error &&
+    !props.model.hotBirdsPromiseState.data
   ) {
     return (
       <Hotlist
         status="loading"
+        onClickHandler = {onClickMoreDetails}
       />
     );
   }
 
   return (
     <Hotlist
-      currentHotlist = {props.model.hotBirdPromiseState.data}
+      currentHotlist = {props.model.hotBirdsPromiseState.data}
+      onClickHandler = {onClickMoreDetails}
       status="data"
     />
   );
