c8a8aa21a5bf854067c5e2485dfcb003e3e00621
anmatar <ahmadmatar8@gmail.com>
2023-12-14 03:34:40 +0100

    update modellen

--- src/model.js
+++ src/model.js
@@ -3,7 +3,7 @@ import resolvePromise from "./resolvePromise";
 import { searchBird } from "./modelSource";
 import { auth } from "./firebaseModel";
 import { signOut } from "firebase/auth";
-import { setCurrentBirdInURL } from './utils';
+
 
 
 export default {
@@ -55,9 +55,6 @@ export default {
     this.updataViewCount(id);
 
     //localStorage.setItem('currentBird', id);
-   // setCurrentBirdInURL(id);
-
-    //setCurrentBirdInURL(id);
    
   },
 
--- src/utils.js
+++
@@ -1,6 +0,0 @@
-export function setCurrentBirdInURL(bird) {
-    const urlParams = new URLSearchParams(window.location.search);
-    urlParams.set('currentBird', bird);
-    const newURL = `${window.location.pathname}?${urlParams.toString()}`;
-    window.history.replaceState({ path: newURL }, '', newURL);
-  }
