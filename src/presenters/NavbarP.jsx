103787bd05cac44fb4c4eede2d93c945b9abd174
Reza Hosseini <rezahos@kth.se>
2023-12-15 11:42:31 +0100

    fix current route not being updated

--- src/presenters/NavbarP.jsx
+++ src/presenters/NavbarP.jsx
@@ -6,29 +6,31 @@ import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 export default observer(function NavbarP({ children, auth, model }) {
   const navigate = useNavigate();
-  const [currentRoute, setCurrentRoute] = useState("/"); // possible routes: home, search, hotlist, mybirds, bird, auth
 
   function onButtonClick(route) {
-    setCurrentRoute(route);
     navigate(route);
   }
   const provider = new GoogleAuthProvider();
-      
-    
+
   async function logInHandlerACB() {
-  try {
+    try {
       const result = await signInWithPopup(auth, provider);
-      console.log('Sign-in successful:', result.user);
-  } catch (error) {
-      console.error('Sign-in error:', error.message);
+      console.log("Sign-in successful:", result.user);
+    } catch (error) {
+      console.error("Sign-in error:", error.message);
+    }
   }
-  }
-  function  logOutHandlerACB(){
-  model.signOut();
+  function logOutHandlerACB() {
+    model.signOut();
   }
 
   return (
-    <Navbar onButtonClick={onButtonClick} currentRoute={currentRoute} logOutHandler ={logOutHandlerACB} logInHandler={logInHandlerACB} user={model.user || null} >
+    <Navbar
+      onButtonClick={onButtonClick}
+      logOutHandler={logOutHandlerACB}
+      logInHandler={logInHandlerACB}
+      user={model.user || null}
+    >
       {children}
     </Navbar>
   );
