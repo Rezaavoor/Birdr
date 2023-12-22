533cf91bfe4a4fe9bf6da74f52169c5a3644735f
anmatar <ahmadmatar8@gmail.com>
2023-12-22 18:02:29 +0100

    update github files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import reactJsxPlugin from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig ({
   plugins: [
     reactJsxPlugin(),
   ],
