83588bc004e519f863841bd9f49e673c342310d8
AhmadNmatar <117222424+AhmadNmatar@users.noreply.github.com>
2023-12-23 11:44:43 +0100

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import reactJsxPlugin from '@vitejs/plugin-react';
+
+export default defineConfig ({
+  plugins: [
+    reactJsxPlugin(),
+  ],
+  server: {
+    port: 8080,
+  },
+  build: {
+    chunkSizeWarningLimit: 2000, 
+  },
+});
